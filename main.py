dc32987cf8b27a5a736da4a8e586c9007bf238a4
Caumannerman <uigohiiming2@gmail.com>
2021-12-06 15:50:45 +0900

    feat: Cut aac file last 10 seconds and save 2 wav files

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from pydub import AudioSegment
 
+#main은 aac파일을 가져와서 10초 이하의 파일로 자르고 그것들을 양 스테레오로 나누어
+# 두개의 wav파일을 저장하는 것 까지
 '''
 audio_file1 = './audio/myaudio(0).aac'
 data1, samplerate1 = librosa.load('./audio/my.m4a')
@@ -19,8 +21,12 @@ plt.show()
 song = AudioSegment.from_('audio/myaudio (1).aac')
 print(song)
 """
+target_path = './audio'
+target_file = '상단소음_상단먼저침.aac'
+save_path = './result/'
+
 stereo_audio = AudioSegment.from_file(
-    './audio/상단소음_상단먼저침.aac',
+    target_path + '/' + target_file,
     format="aac")
 
 # Calling the split_to_mono method
@@ -32,11 +38,24 @@ mono_audios = stereo_audio.split_to_mono()
 # and index 1(right) of list returned
 # by split_to_mono method
 
-mono_left = mono_audios[0].export(
-    "./audio/mono_left.wav",
+print(type(mono_audios))
+print(len(mono_audios))
+print(len(mono_audios[0]))
+print("----------------------------")
+
+if len(mono_audios[0]) > 10000:
+    mono_audios_left = mono_audios[0][-10000:]
+    mono_audios_right = mono_audios[1][-10000:]
+else:
+    mono_audios_left = mono_audios[0]
+    mono_audios_right = mono_audios[1]
+
+
+mono_left = mono_audios_left.export(
+    save_path+target_file[:-4]+"mono_left.wav",
     format="wav")
-mono_right = mono_audios[1].export(
-    "./audio/mono_right.wav",
+mono_right = mono_audios_right.export(
+    save_path+target_file[:-4]+"mono_right.wav",
     format="wav")
 
 print(len(mono_audios))
@@ -44,8 +63,10 @@ print(len(mono_audios[0]))
 print(len(mono_audios[0][0]))
 
 
+# 여기부터는 안해도 됨
 
-data1, samplerate1 = librosa.load('./audio/mono_left.wav')
+
+data1, samplerate1 = librosa.load(save_path+target_file[:-4]+"mono_left.wav")
 print(data1.shape, samplerate1)
 times1 = np.arange(len(data1))/float(samplerate1)
 #sd.play(data,samplerate)
@@ -59,7 +80,7 @@ D = librosa.stft(data1)
 magnitude, phase = librosa.magphase(D)
 
 
-data1, samplerate1 = librosa.load('./audio/mono_right.wav')
+data1, samplerate1 = librosa.load(save_path+target_file[:-4]+"mono_right.wav")
 print(data1.shape, samplerate1)
 times1 = np.arange(len(data1))/float(samplerate1)
 #sd.play(data,samplerate)
