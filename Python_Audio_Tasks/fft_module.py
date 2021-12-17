5538eca8e55b0e45c20f44b8b8a91766ba4aab3e
Caumannerman <uigohiiming2@gmail.com>
2021-12-18 00:01:01 +0900

    Add some Remark for fft_module.py

--- Python_Audio_Tasks/fft_module.py
+++ Python_Audio_Tasks/fft_module.py
@@ -3,9 +3,11 @@ import matplotlib.pyplot as plt
 from scipy.io import wavfile
 from scipy import signal
 import os
+# fft_module은 원본 audio ,필터링 후 audio, fft후 주파수 대역 plot 총 4가지를 plot해줌 (현재는 fft관련 plot은 주석처리)
+# 단순히 filter를 설계하는 과정에서 쓰인 코드
+# 안드로이드에는 fft없이 필터링 하는 코드만 들어간다.
 
-
-wav = "./result/상단소음_상단먼저침mono_left.wav"
+wav = "./result/[6]침대이불위,윗쪽먼저침. 정확도 가장 떨어짐. 너무 강하게 치면 안됐음. 성공률 30%정도mono_right.wav"
 
 #경로 부분과 파일 이름 부분 잘라주는 것  그 뿐
 (file_dir, file_id) = os.path.split(wav)
@@ -33,17 +35,17 @@ magnitude = np.abs(fft)
 f = np.linspace(0,sample_rate, len(magnitude))
 left_spectrum = magnitude[:int(len(magnitude)/2)]
 left_f = f[:int(len(magnitude)/2)]
-
+'''
 plt.figure(figsize=(20,10))
 plt.plot(left_f, left_spectrum)
 plt.xlabel("Frequency")
 plt.ylabel("Magnitude")
 plt.title("Power spectrum")
 plt.show()
+'''
 
 
-
-b = signal.firwin(101, cutoff=(8000,16500), fs=sample_rate, pass_zero='bandpass')
+b = signal.firwin(101, cutoff=(7000,16500), fs=sample_rate, pass_zero='bandpass')
 data1 = signal.lfilter(b,[1,0],data)
 
 fft = np.fft.fft(data1)
@@ -53,12 +55,14 @@ f = np.linspace(0,sample_rate, len(magnitude))
 left_spectrum = magnitude[:int(len(magnitude)/2)]
 left_f = f[:int(len(magnitude)/2)]
 
+'''
 plt.figure(figsize=(20,10))
 plt.plot(left_f, left_spectrum)
 plt.xlabel("Frequency")
 plt.ylabel("Magnitude")
 plt.title("Power spectrum")
 plt.show()
+'''
 
 time = np.linspace(0,len(data1)/sample_rate, len(data1))
 plt.figure(figsize=(20,10))
